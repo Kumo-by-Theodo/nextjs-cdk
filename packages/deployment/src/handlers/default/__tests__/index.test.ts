0cf8cf7bb52d452283bf081441b6b34ef249c49a
guiyom-e <guillaume.egee@outlook.com>
2022-10-21 17:35:21 +0200

    test: add minimal tests for default lambda

---
+++ packages/deployment/src/handlers/default/__tests__/index.test.ts
@@ -0,0 +1,63 @@
+import { Callback, CloudFrontRequestEventRecord, Context } from 'aws-lambda';
+
+import { handler } from '../index';
+
+const mockEmptyContext = {} as Context;
+// eslint-disable-next-line @typescript-eslint/no-empty-function
+const mockVoidCallback = (() => {}) as Callback;
+
+jest.mock('constants/handlerPaths', () => ({
+  RUNTIME_SETTINGS_FILE: './__tests__/mock-runtime-settings.json',
+}));
+
+describe('handler', () => {
+  it('should throw if the request is not defined', () => {
+    expect(() => handler({ Records: [] }, mockEmptyContext, mockVoidCallback)).toThrow();
+  });
+  it('should resolve the request if the uri starts by /_next/', async () => {
+    const cloudFrontEvent = {
+      cf: {
+        request: { uri: '/_next/bob' },
+      },
+    } as unknown as CloudFrontRequestEventRecord;
+
+    await expect(
+      handler({ Records: [cloudFrontEvent] }, mockEmptyContext, mockVoidCallback),
+    ).resolves.toEqual(cloudFrontEvent.cf.request);
+  });
+
+  it('should resolve the request to not found if the uri is not known', async () => {
+    const cloudFrontEvent = {
+      cf: {
+        request: { uri: '/unknown-path' },
+      },
+    } as unknown as CloudFrontRequestEventRecord;
+
+    await expect(
+      handler({ Records: [cloudFrontEvent] }, mockEmptyContext, mockVoidCallback),
+    ).resolves.toEqual({ ...cloudFrontEvent.cf.request, uri: '/serverless/pages/404.html' });
+  });
+
+  it('should resolve the request to a public uri', async () => {
+    const cloudFrontEvent = {
+      cf: {
+        request: { uri: '/favicon.ico' },
+      },
+    } as unknown as CloudFrontRequestEventRecord;
+
+    await expect(
+      handler({ Records: [cloudFrontEvent] }, mockEmptyContext, mockVoidCallback),
+    ).resolves.toEqual({ ...cloudFrontEvent.cf.request, uri: '/public/favicon.ico' });
+  });
+  it('should resolve the request to a static page', async () => {
+    const cloudFrontEvent = {
+      cf: {
+        request: { uri: '/' },
+      },
+    } as unknown as CloudFrontRequestEventRecord;
+
+    await expect(
+      handler({ Records: [cloudFrontEvent] }, mockEmptyContext, mockVoidCallback),
+    ).resolves.toEqual({ ...cloudFrontEvent.cf.request, uri: '/serverless/pages/index.html' });
+  });
+});
